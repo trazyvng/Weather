d58708b5438cc70c346c577cab8cc6c01f08b2f0
trazyvng <trazyngmuniz04@gmail.com>
2024-12-02 15:29:22 -0400

    weather

---
+++ script.js
@@ -0,0 +1,46 @@
+let cityInput = document.getElementById('city_input'),
+searchBtn = document.getElementById('searchBtn'),
+api_key = '9b2f690c578eb3ca7fa3e9aa5a03468f';
+
+function getWeatherDetails(name, lat, lon, country, state){
+    let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={api_key}`,
+    WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={api_key}`,
+    days = [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miercoles',
+        'Jueves',
+        'Viernes',
+        'Sabado'
+    ],
+    months = [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+    ]
+}
+
+function getCityCoordinates(){
+    let cityName = cityInput.value.trim();
+    cityInput.value = '';
+    if(!cityName) return;
+    let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`;
+    fetch(GEOCODING_API_URL).then(res=> res.json()).then(data => {
+        let {name, lat, lon, country, state} = data[0];
+        getWeatherDetails(name, lat, lon, country, state);
+    }).catch(() => {
+        alert(`Failed to fetch coordinates of ${cityName}`);
+    })
+}
+
+searchBtn.addEventListener('click', getCityCoordinates);
