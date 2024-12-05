8d0d4be6349c6e2fbb91a558e23c28494a478ab7
trazyvng <trazyngmuniz04@gmail.com>
2024-12-05 15:00:58 -0400

    Fix: Corrected code in script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let cityInput = document.getElementById('city_input'),
 searchBtn = document.getElementById('searchBtn'),
 api_key = '9b2f690c578eb3ca7fa3e9aa5a03468f';
+currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
 
 function getWeatherDetails(name, lat, lon, country, state){
-    let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={api_key}`,
-    WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={api_key}`,
+    let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
     days = [
         'Domingo',
         'Lunes',
@@ -27,7 +28,30 @@ function getWeatherDetails(name, lat, lon, country, state){
         'Octubre',
         'Noviembre',
         'Diciembre'
-    ]
+    ];
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        let date = new Date(); 
+        currentWeatherCard.innerHTML = `
+            <div class="current-weather">
+                <div class="details">
+                    p>Ahora</p>
+                    <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
+                    <p>${(data.weather[0].description)}</p>
+                </div>
+                <div class="weather-icon">
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Nublado">
+                </div>
+                </div>
+                <hr>
+                <div class="card-footer">
+                    <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}</p>
+                    <p><i class="fa-light fa-location-dot"></i> ${name}, ${country}</p>
+                </div>
+        `;
+    }).catch(() => {
+        alert('Failed to fetch current weather');
+    })
 }
 
 function getCityCoordinates(){
